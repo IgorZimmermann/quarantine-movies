fb4a304de8ac53cff0f99db346c7936aa1074a55
PaperKing01 <neumann.technologies@gmail.com>
2020-04-13 00:01:28 +0200

    Hello World with Pug

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+app.use('/p/', express.static(__dirname + '/public/'));
+app.set('views', __dirname + '/views/');
+
 app.get('/', (req, res) => {
-	res.send('Hello World');
+	res.render('index');
 });
 
 app.listen(1919, () => {
