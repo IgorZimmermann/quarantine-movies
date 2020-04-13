a7cabb9429eb542e89c25e9972e0166e1576e9d8
PaperKing01 <neumann.technologies@gmail.com>
2020-04-13 13:15:29 +0200

    Working app, no styling

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
-const request = require('request');
+const superagent = require('superagent');
 const cheerio = require('cheerio');
 
 const bodyParser = require('body-parser');
@@ -33,37 +33,91 @@ app.get('/movie/:m/:d/', (req, res) => {
 	res.send(data);
 });
 
-app.post('/add/:m/:d/', (req, res) => {
+app.post('/add/:m/:d/', async (req, res) => {
+	let s = JSON.parse(fs.readFileSync(__dirname + '/storage.json'));
 	let title = req.body.title;
+	let target = s[req.params.m].days[req.params.d];
+	target.title = title;
+	fs.writeFileSync(__dirname + '/storage.json', JSON.stringify(s));
+	console.log('1');
+	res.redirect('/');
+	console.log('2');
 	let uri = `https://hu.wikipedia.com/wiki/${encodeURI(
 		title.replace(/ /g, '_')
 	)}`;
-	request(uri, (e, r, b) => {
-		let $ = cheerio.load(b);
-		if ($('h1#firstHeading').text().includes('egyértelműsítő lap')) {
-			request(uri + '_(film)', (er, re, bo) => {
-				let $$ = cheerio.load(bo);
-				let small = $$('td.fejlec small');
-				let srcTitle;
-				if (small.length != 0) {
-					console.log('small');
-					srcTitle = small.text().replace('(', '').replace(')', '');
-				} else {
-					srcTitle = $$('td.fejlec').text().replace('(', '').replace(')', '');
-				}
-				res.send(srcTitle);
-			});
-		} else {
-			let small = $('td.fejlec small');
-			let srcTitle;
-			if (small) {
-				srcTitle = small.text().replace('(', '').replace(')', '');
-			} else {
-				srcTitle = $('td.fejlec').text().replace('(', '').replace(')', '');
-			}
-			res.send(srcTitle);
+	let body = await superagent.get(uri);
+	let $ = cheerio.load(body.text);
+	if ($('#firstHeading').text().includes('egyértelműsítő')) {
+		try {
+			body = await superagent.get(uri + '_(film)');
+		} catch (e) {
+			body = await superagent.get(`${uri}_(film, ${req.body.date})`);
 		}
-	});
+		$ = cheerio.load(body.text);
+	}
+	let srcTitle;
+	let small = $('.fejlec small');
+	if (small) {
+		srcTitle = small.text().replace('(', '').replace(')', '');
+	} else {
+		srcTitle = $('.fejlec').text();
+	}
+	uri = `https://imdb.com/find?q=${encodeURI(srcTitle)}`;
+	body = await superagent.get(uri);
+	$ = cheerio.load(body.text);
+	uri =
+		'https://imdb.com' +
+		$('.findResult:first-of-type .result_text a').attr('href');
+	body = await superagent.get(uri);
+	$ = cheerio.load(body.text);
+	let data = {
+		title: title,
+		directors: [],
+		writers: [],
+		stars: [],
+		trailer: 'https://imdb.com' + $("a[data-type='recommends']").attr('href'),
+		cover: $('.poster img').attr('src'),
+		released: $(".subtext a[title='See more release dates']")
+			.text()
+			.replace(/(?:\n)/g, ''),
+		length: $('.subtext time').text().replace(/\s+/g, ''),
+		rating: $("span[itemprop='ratingValue']").text(),
+	};
+	let creditArray = $('.credit_summary_item');
+	creditArray
+		.eq(0)
+		.find('a')
+		.each((i, e) => {
+			let d = {
+				name: $(e).text(),
+				imdb: $(e).attr('href').split('?')[0],
+			};
+			data.directors.push(d);
+		});
+	creditArray
+		.eq(1)
+		.find('a')
+		.each((i, e) => {
+			let d = {
+				name: $(e).text(),
+				imdb: $(e).attr('href').split('?')[0],
+			};
+			data.writers.push(d);
+		});
+	creditArray
+		.eq(2)
+		.find('a')
+		.each((i, e) => {
+			if ($(e).text() == 'See full cast & crew') return;
+			let d = {
+				name: $(e).text(),
+				imdb: $(e).attr('href').split('?')[0],
+			};
+			data.stars.push(d);
+		});
+	target = data;
+	fs.writeFileSync(__dirname + '/storage.json', JSON.stringify(s));
+	console.log('3');
 });
 
 app.listen(1919, () => {
