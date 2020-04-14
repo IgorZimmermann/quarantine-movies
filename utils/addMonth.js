fcb74aa078b0082f6d1e475f8d9d7b5452ac534b
PaperKing01 <neumann.technologies@gmail.com>
2020-04-14 14:24:30 +0200

    Created 'addMonth' util

---
+++ utils/addMonth.js
@@ -0,0 +1,15 @@
+const { readFileSync, writeFileSync } = require('fs');
+const path = require('path');
+let s = JSON.parse(readFileSync(path.dirname(__dirname) + '\\storage.json'));
+
+s[process.argv[2]] = {
+	display: process.argv[3],
+	number: process.argv[4],
+	days: {},
+};
+
+for (var i = 0; i <= process.argv[5]; i++) {
+	s[process.argv[2]].days[i] = {};
+}
+
+writeFileSync(path.dirname(__dirname) + '\\storage.json', JSON.stringify(s));
