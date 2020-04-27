90ff0c0b5b5b6643f2fa29b67c13e9c894f68424
Igor Zimmermann <igorzimmermann01@gmail.com>
2020-04-27 09:40:13 +0200

    HBO util checks for date

--- utils/HBO.js
+++ utils/HBO.js
@@ -6,8 +6,17 @@ module.exports = async (reqBody) => {
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0')
   await page.goto('https://hbogo.hu/search/')
   await page.type('#search-input', reqBody.title)
-  await page.waitFor(3000)
+  await page.waitFor(1000)
   let movHREF = await page.evaluate(() => document.querySelectorAll('.search-item a')[0].getAttribute('href'))
-  await browser.close()
-  return `https://hbogo.hu${movHREF}`
+  await page.goto("https://hbogo.hu"+movHREF)
+  await page.waitFor(1000)
+  let isValid = await page.evaluate((reqBody) => {
+    let metaArray = document.querySelectorAll('.meta')[0].textContent.split('|')
+    if (metaArray[0].replace(/[A-Z]| /g, '') == reqBody.date) {
+      return true
+    }
+  }, reqBody)
+  if (isValid) {
+    return movHREF
+  }
 }
