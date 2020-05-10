2641bf1d31802ed2b5a6951a0398b37237538e43
Igor Zimmermann <igorzimmermann01@gmail.com>
2020-05-10 20:24:30 +0200

    Wait for page initialisation in getHighResImage.js

--- utils/getHighResImage.js
+++ utils/getHighResImage.js
@@ -4,6 +4,7 @@ module.exports = async (reqBody) => {
   const browser = await puppeteer.launch({ executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe' })
   const page = await browser.newPage()
   await page.goto(`https://www.google.com/search?q=${encodeURI(reqBody.title).replace(/%20/g, '+')}+${reqBody.date}+poster&ijn=0&tbm=isch&tbs=isz%3Al%2Ciar%3At`)
+  await page.waitForSelector('[data-ri="0"] a:nth-child(1)')
   await page.click('[data-ri="0"] a:nth-child(1)');
   let src = await page.evaluate(() => document.querySelector('[data-ri="0"] a:nth-child(1)').getAttribute('href'))
   await browser.close()
