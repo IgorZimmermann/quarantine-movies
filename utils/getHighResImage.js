fc547897581ffcc62aaec9aed235ea5917a92a97
Igor Zimmermann <igorzimmermann01@gmail.com>
2020-05-12 11:10:54 +0200

    Changed high res image query

--- utils/getHighResImage.js
+++ utils/getHighResImage.js
@@ -2,6 +2,6 @@ const puppeteer = require('puppeteer')
 
 module.exports = ($) => {
   let splitSRC = $('.poster img').attr('src').split('@')
-  splitSRC[splitSRC.length - 1] = '._V1_SY1000_CR0,0,686,1000_AL_.jpg'
+  splitSRC[splitSRC.length - 1] = '._V1.jpg'
   return splitSRC.join('@')
 }
